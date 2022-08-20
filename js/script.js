6fcee8b50b3fb1a9ccc0e9bcdc5e12b72a4f111a
Mohd Saif <mohdsaifb173@gmail.com>
2022-08-20 15:01:00 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ======================= Typing Animation ====================== */
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","App Developer","Programmer","Gamer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
